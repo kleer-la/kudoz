ac463b4f52b8a6b7b28fc8e182c0bdecbecd4cd6
Martin Alaimo <malaimo@gmail.com>
2014-01-30 00:24:26 -0300

    fix migration

--- db/migrate/014_add_kudozio_the_great.rb
+++ db/migrate/014_add_kudozio_the_great.rb
@@ -3,9 +3,6 @@ class AddKudozioTheGreat < ActiveRecord::Migration
     change_table :users do |t|
       t.boolean :is_kudozio
     end
-    
-    User.create( fname: "Kudozio", lname: "The Great", accounts: [ Account.create( balance: 0 ) ], image_url: "/img/kudozio/avatar.png", is_kudozio: true )
-    
   end
 
   def self.down
