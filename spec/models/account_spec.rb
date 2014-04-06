7f6dfefedbd570d88e9d1d0e182c7a2d440f0865
Carlos Peix <carlos.peix@gmail.com>
2014-04-06 13:04:09 -0300

    Running tests just with AR, in memory

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'support/active_record'
 
 describe Account do
   
--- spec/models/feedback_cycle_position_spec.rb
+++ spec/models/feedback_cycle_position_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'support/active_record'
 
 describe FeedbackCyclePosition do
 end
